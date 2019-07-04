9e6964d754593377013dd7590d7c8b09f7721f26
Anh Le <anhqle@kms-technology.com>
2019-07-04 10:50:32 +0700

    remove license

--- src/main/java/com/katalon/license/ComponentUtils.java
+++ src/main/java/com/katalon/license/ComponentUtils.java
@@ -2,18 +2,18 @@ package com.katalon.license;
 
 
 public class ComponentUtils {
-
-  private static LicenseUtils licenseUtils;
-
-//  public static <T> T getComponent(Class<T> clazz) {
-//    return ComponentManager.getComponentInstanceOfType(clazz);
+//
+//  private static LicenseUtils licenseUtils;
+//
+////  public static <T> T getComponent(Class<T> clazz) {
+////    return ComponentManager.getComponentInstanceOfType(clazz);
+////  }
+//
+//  public static LicenseUtils getLicenseUtils() {
+//    return licenseUtils;
 //  }
-
-  public static LicenseUtils getLicenseUtils() {
-    return licenseUtils;
-  }
-
-  public static void setLicenseUtils(LicenseUtils licenseUtils) {
-    ComponentUtils.licenseUtils = licenseUtils;
-  }
+//
+//  public static void setLicenseUtils(LicenseUtils licenseUtils) {
+//    ComponentUtils.licenseUtils = licenseUtils;
+//  }
 }
--- src/main/java/com/katalon/license/LicenseUtils.java
+++ src/main/java/com/katalon/license/LicenseUtils.java
@@ -4,32 +4,32 @@ import com.atlassian.upm.api.license.PluginLicenseManager;
 import com.atlassian.upm.api.license.entity.PluginLicense;
 import com.atlassian.upm.api.util.Option;
 import org.osgi.service.component.annotations.Component;
-
-@Component
-public class LicenseUtils {
-
-  private PluginLicenseManager pluginLicenseManager;
-
-  public LicenseUtils(PluginLicenseManager pluginLicenseManager) {
-    ComponentUtils.setLicenseUtils(this);
-    this.pluginLicenseManager = pluginLicenseManager;
-  }
-
-  public LicenseValidation validateLicense() {
-    LicenseValidation licenseValidation = new LicenseValidation();
-    boolean valid;
-    try {
-      Option<PluginLicense> licenseOption = pluginLicenseManager.getLicense();
-      if (licenseOption.isDefined()) {
-        PluginLicense license = licenseOption.get();
-        valid = license != null && license.isValid();
-      } else {
-        valid = false;
-      }
-    } catch (Exception e) {
-      valid = false;
-    }
-    licenseValidation.setValid(valid);
-    return licenseValidation;
-  }
-}
+//
+//@Component
+//public class LicenseUtils {
+//
+//  private PluginLicenseManager pluginLicenseManager;
+//
+//  public LicenseUtils(PluginLicenseManager pluginLicenseManager) {
+//    ComponentUtils.setLicenseUtils(this);
+//    this.pluginLicenseManager = pluginLicenseManager;
+//  }
+//
+//  public LicenseValidation validateLicense() {
+//    LicenseValidation licenseValidation = new LicenseValidation();
+//    boolean valid;
+//    try {
+//      Option<PluginLicense> licenseOption = pluginLicenseManager.getLicense();
+//      if (licenseOption.isDefined()) {
+//        PluginLicense license = licenseOption.get();
+//        valid = license != null && license.isValid();
+//      } else {
+//        valid = false;
+//      }
+//    } catch (Exception e) {
+//      valid = false;
+//    }
+//    licenseValidation.setValid(valid);
+//    return licenseValidation;
+//  }
+//}
